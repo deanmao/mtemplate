e4953f0992d59d395cfc8d7195d65d82cf557b18
Dean Mao <deanmao@gmail.com>
2023-02-24 22:15:38 -0800

    add rtl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import NextApp, { AppProps, AppContext } from 'next/app';
 import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head';
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { MantineProvider, ColorScheme, ColorSchemeProvider, Slider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { rtlCache } from '../rtl-cache';
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
@@ -23,13 +24,21 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          <NotificationsProvider>
-            <Component {...pageProps} />
-          </NotificationsProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <div dir="rtl">
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider
+            theme={{ colorScheme, dir: 'rtl' }}
+            withGlobalStyles
+            withNormalizeCSS
+            emotionCache={rtlCache}
+          >
+            <NotificationsProvider>
+              <Component {...pageProps} />
+              <Slider defaultValue={30} />
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </div>
     </>
   );
 }
